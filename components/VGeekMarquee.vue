7b51f61e0bfecaf475b3c4d43213ccb71827498f
崎津 祥汰 <shota.sakitsu@virtlive.jp>
2025-07-08 13:20:19 +0900

    マーキーの幅ズレ問題を修正しました
    - BlinkやWebKitで静止状態のマーキー幅がズレる問題に対応しました。
    - 各ブラウザで異なる`letter-spacing`スタイルを適用するロジックを追加しました。
    - CSSに`margin-inline`の調整を加え、デザインを微修正しました。

--- components/VGeekMarquee.vue
+++ components/VGeekMarquee.vue
@@ -7,13 +7,30 @@ const motionReduced = computed(() => reduceMotion.value || printPaper.value);
 const breakpoint = useBreakpoints(breakpointsTailwind);
 const xl = breakpoint.greaterOrEqual("xl");
 const md = breakpoint.smallerOrEqual("md");
+const container = useTemplateRef("container");
+const addStyle = computed(() => {
+	// BlinkやWebKitでマーキーが静止状態の時に幅がズレる問題を修正
+	// ChromeやEdge (Blink)を引っかける
+	if (navigator.userAgent.toLowerCase().includes("applewebkit/537.36")) {
+		console.log("Chrome or Edge (Blink) Marquee Width fix (letter-spacing: 0.15vw;)")
+		return "letter-spacing: 0.15vw;"
+	}
+	// Safari (WebKit)を引っかける
+	if (navigator.userAgent.toLowerCase().includes("applewebkit")) {
+		console.log("Safari (WebKit) Marquee Width fix (letter-spacing: -0.12vw;)")
+		return "letter-spacing: -0.12vw;"
+	}
+	// Firefox (Gecko)などはUA的に引っかからないはず
+	return "";
+})
 </script>
 
 <template>
-	<div :class="`vgeek-marquee-container ${motionReduced ? 'motion-reduced ' : ''} ${xl || md ? `breakpoint-${xl ? 'xl' : 'md'}` : ''}`">
+	<div ref="container" :class="`vgeek-marquee-container ${motionReduced ? 'motion-reduced ' : ''} ${xl || md ? `breakpoint-${xl ? 'xl' : 'md'}` : ''}`">
 		<client-only>
 			<Vue3Marquee :duration="motionReduced ? 0 : 50">
 				<span
+					:style="addStyle"
 					:class="`vgeek-marquee tw:text-stone-500/75 ${motionReduced ? 'motion-reduced ' : ''} ${xl || md ? `breakpoint-${xl ? 'xl' : 'md'}` : ''}`">
 					Vgeek Production
 				</span>
@@ -47,6 +64,7 @@ const md = breakpoint.smallerOrEqual("md");
 	display: inline-block;
 	font-weight: bold;
 	font-size: 50vw;
+	margin-inline: .5ex;
 
 	&.breakpoint-xl {
 		font-size: 15vw;
@@ -54,6 +72,7 @@ const md = breakpoint.smallerOrEqual("md");
 
 	&.motion-reduced {
 		font-size: 11.5vw;
+		margin-inline: unset;
 
 		&.breakpoint-md {
 			font-size: calc(calc(100vh - calc(var(--tw-spacing) * 40)) / 100 * 11);
